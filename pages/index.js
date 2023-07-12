d000342b96a863b397628bac072abc6f1df11d71
linweihao <linweihao@boulderaitech.com>
2023-07-13 01:39:53 +0800

    feat:next的路由使用

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <p>Read <Link href="/posts/first-post">this page!</Link></p>
         </h1>
 
         <p className={styles.description}>
