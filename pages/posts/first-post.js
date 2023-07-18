f576fe3441ff8dfad0849674f01207105b85ec3f
hewitt99 <1686076624@qq.com>
2023-07-19 00:05:33 +0800

    feat:next/head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 import ImageComp from '/components/ImageComp'
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>FIRST-POST</title>
+      </Head>
       <h1>first-post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
